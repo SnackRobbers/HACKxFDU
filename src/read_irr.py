082a8d87e3627eff16becdda79611ca1e98f987e
Soros Liu <soros.liu1029@gmail.com>
2016-10-16 05:46:16 +0800

    test output

--- src/read_irr.py
+++ src/read_irr.py
@@ -7,4 +7,4 @@ p2 = subprocess.Popen(args, stdout=subprocess.PIPE)
 while True:
     rlist, wlist, xlist = select.select([p1.stdout, p2.stdout], [], [])
     for stdout in rlist:
-        sys.stdout.write(os.read(stdout.fileno(), 1024).decode('utf-8'))
+        sys.stdout.write('indicator: ' + os.read(stdout.fileno(), 1024).decode('utf-8'))
