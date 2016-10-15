fd709ef2bdc2ab02f0878ef046e339d88bb8ca78
irmowan <irmowan@gmail.com>
2016-10-15 16:05:18 +0800

    Call LUIS API

---
+++ src/LUIS.py
@@ -0,0 +1,21 @@
+import requests
+import sys
+
+
+def callAPI(text):
+    url = 'https://api.projectoxford.ai/luis/v1/application' + \
+          '?id=bd9d9b8f-ac87-4bc9-94de-f3aeb734ead6&subscription-key=86ef114a9ce94ca9a56bda305d09b1b4'
+    request_url = url + '&q=' + text
+    r = requests.get(request_url)
+    if r.status_code != 200:
+        raise ConnectionError
+    print(r.json())
+
+
+def main():
+    text = input('Please input a sentence in Chinese: ')
+    callAPI(text)
+
+
+if __name__ == '__main__':
+    sys.exit(int(main() or 0))
