c23acbb70c42049e98dbf951c07322c46ff74de3
Soros Liu <soros.liu1029@gmail.com>
2016-10-16 09:08:59 +0800

    update led

--- src/led.py
+++ src/led.py
@@ -8,6 +8,7 @@ class TrafficLight:
             for p in pins:
                 GPIO.setup(p, GPIO.OUT)
         self.r_pwm, self.y_pwm, self.g_pwm = [], [], []
+        self.initPWM(r_pins, y_pins, g_pins)
 
     def initPWM(self, r_pins, y_pins, g_pins):
         for _pins, _pwm in zip([r_pins, y_pins, g_pins], [self.r_pwm, self.y_pwm, self.g_pwm]):
@@ -17,43 +18,43 @@ class TrafficLight:
                 _pwm.append(pwm)
 
     def red_up(self):
-        self.r_pwm[0].ChangeDutyCycle(0)
-        self.r_pwm[1].ChangeDutyCycle(100)
-        self.r_pwm[2].ChangeDutyCycle(100)
-
-        self.y_pwm[0].ChangeDutyCycle(100)
-        self.y_pwm[1].ChangeDutyCycle(100)
-        self.y_pwm[2].ChangeDutyCycle(100)
-
-        self.g_pwm[0].ChangeDutyCycle(100)
-        self.g_pwm[1].ChangeDutyCycle(100)
-        self.g_pwm[2].ChangeDutyCycle(100)
-
-    def yellow_up(self):
         self.r_pwm[0].ChangeDutyCycle(100)
-        self.r_pwm[1].ChangeDutyCycle(100)
-        self.r_pwm[2].ChangeDutyCycle(100)
+        self.r_pwm[1].ChangeDutyCycle(0)
+        self.r_pwm[2].ChangeDutyCycle(0)
 
         self.y_pwm[0].ChangeDutyCycle(0)
         self.y_pwm[1].ChangeDutyCycle(0)
-        self.y_pwm[2].ChangeDutyCycle(100)
+        self.y_pwm[2].ChangeDutyCycle(0)
 
-        self.g_pwm[0].ChangeDutyCycle(100)
-        self.g_pwm[1].ChangeDutyCycle(100)
-        self.g_pwm[2].ChangeDutyCycle(100)
+        self.g_pwm[0].ChangeDutyCycle(0)
+        self.g_pwm[1].ChangeDutyCycle(0)
+        self.g_pwm[2].ChangeDutyCycle(0)
 
-    def green_up(self):
+    def yellow_up(self):
         self.r_pwm[0].ChangeDutyCycle(0)
-        self.r_pwm[1].ChangeDutyCycle(100)
-        self.r_pwm[2].ChangeDutyCycle(100)
+        self.r_pwm[1].ChangeDutyCycle(0)
+        self.r_pwm[2].ChangeDutyCycle(0)
 
         self.y_pwm[0].ChangeDutyCycle(100)
         self.y_pwm[1].ChangeDutyCycle(100)
-        self.y_pwm[2].ChangeDutyCycle(100)
+        self.y_pwm[2].ChangeDutyCycle(0)
 
-        self.g_pwm[0].ChangeDutyCycle(100)
+        self.g_pwm[0].ChangeDutyCycle(0)
         self.g_pwm[1].ChangeDutyCycle(0)
-        self.g_pwm[2].ChangeDutyCycle(100)
+        self.g_pwm[2].ChangeDutyCycle(0)
+
+    def green_up(self):
+        self.r_pwm[0].ChangeDutyCycle(0)
+        self.r_pwm[1].ChangeDutyCycle(0)
+        self.r_pwm[2].ChangeDutyCycle(0)
+
+        self.y_pwm[0].ChangeDutyCycle(0)
+        self.y_pwm[1].ChangeDutyCycle(0)
+        self.y_pwm[2].ChangeDutyCycle(0)
+
+        self.g_pwm[0].ChangeDutyCycle(0)
+        self.g_pwm[1].ChangeDutyCycle(100)
+        self.g_pwm[2].ChangeDutyCycle(0)
 
     def clean_up(self):
         for _pwm in [self.r_pwm, self.y_pwm, self.g_pwm]:
--- src/test_led.py
+++ src/test_led.py
@@ -1,6 +1,14 @@
-import led
-import led_config
+from led import TrafficLight
+from led_config import config_led
+from time import sleep
 
-config = led_config()
+config = config_led()
 traffic_lights = TrafficLight(*config)
-traffic_lights.red_up()
+sleep(2)
+while True:
+    traffic_lights.red_up()
+    sleep(0.5)
+    traffic_lights.yellow_up()
+    sleep(0.5)
+    traffic_lights.green_up()
+    sleep(0.5)
