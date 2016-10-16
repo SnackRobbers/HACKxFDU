20474d00d303377ba03f434f92d10d9fd0c38456
Soros Liu <soros.liu1029@gmail.com>
2016-10-16 10:43:48 +0800

    add relay to read_irr function

--- src/read_irr.py
+++ src/read_irr.py
@@ -1,31 +1,34 @@
 import os, sys, select, subprocess
+from relay import RelayLED
 
-args = ['mode2', '-d', '/dev/lirc0']
-p1 = subprocess.Popen(args, stdout=subprocess.PIPE)
-p2 = subprocess.Popen(args, stdout=subprocess.PIPE)
+def read_irr():
+    args = ['mode2', '-d', '/dev/lirc0']
+    p1 = subprocess.Popen(args, stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(args, stdout=subprocess.PIPE)
 
-space_list = []
-pulse_list = []
-try:
-    while True:
-        rlist, wlist, xlist = select.select([p1.stdout, p2.stdout], [], [])
-        for stdout in rlist:
-            output = os.read(stdout.fileno(), 1024).decode('utf-8')
+    space_list = []
+    pulse_list = []
+    led = RelayLED()
+    try:
+        while True:
+            rlist, wlist, xlist = select.select([p1.stdout, p2.stdout], [], [])
+            for stdout in rlist:
+                output = os.read(stdout.fileno(), 1024).decode('utf-8')
 
-            if output:
-                # sys.stdout.write(output)
-                # sys.stdout.write('indicator: ' + output)
-                o = output.split()
-                if len(o) == 4:
-                    for wave, length in zip(o[::2], o[1::2]):
-                        if wave == 'space':
-                            space_list.append(int(length))
-                        elif wave == 'pulse':
-                            pulse_list.append(int(length))
+                if output:
+                    # sys.stdout.write(output)
+                    # sys.stdout.write('indicator: ' + output)
+                    o = output.split()
+                    if len(o) == 4:
+                        for wave, length in zip(o[::2], o[1::2]):
+                            if wave == 'space':
+                                space_list.append(int(length))
+                            elif wave == 'pulse':
+                                pulse_list.append(int(length))
 
-                    if len(space_list) == 36 and len(pulse_list) == 36:
-                        pass
+                        if len(space_list) == 36 and len(pulse_list) == 36:
+                            led.toggle()
 
-except KeyboardInterrupt:
-    print('Exist Now....')
-    sys.exit(0)
+    except KeyboardInterrupt:
+        print('Exist Now....')
+        sys.exit(0)
